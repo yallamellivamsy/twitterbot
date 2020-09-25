4b09191478e8adc46193670a2830601b2ce8abb9
yallamellivamsy <yallamellimadhav@gmail.com>
2020-09-25 11:47:47 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const twitterApp = {
+  
+consumer_key: 'Y52FlgacfLPWHrbNWkuldzPGh',
+  
+consumer_secret: 'Raxul13dTIzWYCkjPIvXqvFXC9TzmmXqEMUONr7UytxCSa32Ko',
+  
+access_token: '2356618602-KmhsQakKPvWIjNPU4M0mexo3bCC3fXMPSi63QmI',
+  
+access_token_secret: 'cRinIrTqhrCz7VUaVDSjkCt0C51MQyImSK4KP3936cGwI',
+  
+timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+
+};
+
+
+module.exports = {
+  twitterApp,
+  userName: '@yallamellivamsy'
+};
